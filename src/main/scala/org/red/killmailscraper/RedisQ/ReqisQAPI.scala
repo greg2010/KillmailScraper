616e18eb055d8f38f3f4ef4ea95ba593887b029f
Grigory Romodanovskiy <grigory@weme.sh>
2017-02-21 15:20:58 -0500

    fix tail recursion

--- src/main/scala/org/red/killmailscraper/RedisQ/ReqisQAPI.scala
+++ src/main/scala/org/red/killmailscraper/RedisQ/ReqisQAPI.scala
@@ -9,6 +9,7 @@ import org.red.killmailscraper.RedisQ.RedisQSchema.RedisQJsonDeserializer._
 import spray.json._
 import org.red.killmailscraper.scraperConfig
 
+import scala.annotation.tailrec
 import scala.concurrent.duration._
 import scala.util.control.NonFatal
 
@@ -22,37 +23,14 @@ class ReqisQAPI(queueId: String) extends LazyLogging {
   def poll(): KillPackage = {
     val httpClient = SimpleHttp1Client()
     val getKillmail = httpClient.expect[String](url)
-    def next(tolerance: FiniteDuration): KillPackage = {
-      try {
+    @tailrec def next(): KillPackage = {
         val response = getKillmail.unsafePerformSyncFor((scraperConfig.getInt("ttw") + 2).seconds)
           .parseJson.convertTo[RootPackage]
           response.`package` match {
           case Some(x) => x
-          case None => {
-            if (tolerance <= 100.milliseconds) next(tolerance)
-            else next(tolerance / 2)
-          }
+          case None => next()
         }
-      } catch {
-        case ex: UnexpectedStatus if ex.status.code == 429 => {
-          val sleepTime: Int = scraperConfig.getInt("ttw") / 2
-          logger.warn(s"Got unexpected status exception, sleeping for ${sleepTime.seconds.toMillis} milliseconds...", ex)
-          Thread.sleep(sleepTime.seconds.toMillis)
-          next(tolerance)
-        }
-        case ex: DeserializationException => {
-          logger.warn(s"Error deserializing json object, cause: ${ex.cause}" +
-            s" fieldNames: ${ex.fieldNames} message: ${ex.msg}")
-          next(tolerance)
-        }
-        case ex if NonFatal(ex) => {
-          logger.error(s"General run exception, sleeping for ${tolerance.toMillis} milliseconds", ex)
-          Thread.sleep(tolerance.toMillis)
-          if (tolerance > 10.seconds) next(tolerance)
-          else next(tolerance * 2)
-        }
-      }
     }
-    next(500.milliseconds)
+    next()
   }
 }
--- src/main/scala/org/red/killmailscraper/ScraperController.scala
+++ src/main/scala/org/red/killmailscraper/ScraperController.scala
@@ -1,19 +1,44 @@
 package org.red.killmailscraper
 
+import com.typesafe.scalalogging.LazyLogging
+import org.http4s.client.UnexpectedStatus
 import org.red.killmailscraper.RedisQ.RedisQSchema.KillPackage
 import org.red.killmailscraper.db.DBController
+import spray.json.DeserializationException
 
-class ScraperController {
+import scala.annotation.tailrec
+import scala.util.control.NonFatal
+import scala.concurrent.duration._
+
+class ScraperController extends LazyLogging {
 
   def run(): Unit = {
     val redisQApi = new RedisQ.ReqisQAPI(scraperConfig.getString("queueID"))
 
-    def iterate(): Unit = {
+    @tailrec def iterate(): Unit = {
       val response: KillPackage = redisQApi.poll()
       DBController.pushToDB(response)
       iterate()
     }
 
-    iterate()
+    try {
+      iterate()
+    } catch {
+      case ex: UnexpectedStatus if ex.status.code == 429 => {
+        val sleepTime: Int = scraperConfig.getInt("ttw") / 2
+        logger.warn(s"Got unexpected status exception, sleeping for ${sleepTime.seconds.toMillis} milliseconds...", ex)
+        Thread.sleep(sleepTime.seconds.toMillis)
+        iterate()
+      }
+      case ex: DeserializationException => {
+        logger.warn(s"Error deserializing json object, cause: ${ex.cause}" +
+          s" fieldNames: ${ex.fieldNames} message: ${ex.msg}")
+        iterate()
+      }
+      case ex if NonFatal(ex) => {
+        logger.error(s"General run exception", ex)
+        iterate()
+      }
+    }
   }
 }
